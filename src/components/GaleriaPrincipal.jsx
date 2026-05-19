d24ca25c9e5f849f6de702502682d8681d234f9b
ERAM4 <maa.rojasp@duocuc.cl>
2026-05-19 09:49:40 -0400

    Limpieza de codigo 2

--- src/components/GaleriaPrincipal.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import GaleriaAdmin from '../components/GaleriaAdmin';
-import GaleriaPublica from '../components/GaleriaPublica';
-
-export default function GaleriaPrincipal() {
-  const rol = localStorage.getItem('rolUsuario');
-  return rol === 'ADMIN' ? <GaleriaAdmin /> : <GaleriaPublica />;
-}
--- src/pages/servicios.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ServiciosPagina from '../components/serviciosPagina.jsx'
-function servicios() {
-  return (
-    <ServiciosPagina/>
-  )
-}
-
-export default servicios
